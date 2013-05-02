21f8b9dccc0b32252297a01fa4bac6f8e14fbdc9
Kiall Mac Innes <kiall@managedit.ie>
2013-05-02 12:13:09 +0100

    Support loading parsers via EntryPoints

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import textwrap
+
 
 setup(
     name='logster',
@@ -21,4 +23,14 @@ setup(
         'bin/logster'
     ],
     license='GPL3',
+    entry_points=textwrap.dedent("""
+        [logster.parsers]
+        ErrorLogLogster = logster.parsers.ErrorLogLogster:ErrorLogLogster
+        Log4jLogster = logster.parsers.Log4jLogster:Log4jLogster
+        MetricLogster = logster.parsers.MetricLogster:MetricLogster
+        PostfixLogster = logster.parsers.PostfixLogster:PostfixLogster
+        SampleLogster = logster.parsers.SampleLogster:SampleLogster
+        SquidLogster = logster.parsers.SquidLogster:SquidLogster
+    """),
+
 )
